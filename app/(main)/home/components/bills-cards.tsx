6c68bbee17dd26456505c79f4db1444e1970f738
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2024-07-10 13:28:31 +0300

    chore: Update bills-cards component with lazy loading for images

--- app/(main)/home/components/bills-cards.tsx
+++ app/(main)/home/components/bills-cards.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { ArrowUpDown, Phone, Smartphone } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -6,24 +9,30 @@ const BillsCards = () => {
   return (
     <>
       <div className="flex w-full flex-row ">
-        <div className="w-full h-32 border mb-3 bg-white rounded-md flex flex-col items-center p-3">
-          <div className="flex flex-1 items-center"></div>
+        <div className="w-full h-32 border mb-3 bg-white rounded-md flex flex-col items-center pb-2 overflow-hidden">
+          <div className="flex h-full w-full items-center relative bg-black overflow-hidden">
+            <Image src="/icons/phone.png" layout="fill" alt="phone" />
+          </div>
           <p className="text-xs font-semibold text-center">Airtime & Data</p>
         </div>
 
         <Link
           href="/tv"
-          className="w-full h-32 border bg-white mx-3 rounded-md flex flex-col items-center p-3"
+          className="w-full h-32 border mb-3 bg-white rounded-md flex flex-col items-center pb-2 overflow-hidden mx-3"
         >
-          <div className="flex flex-1"></div>
-          <p className="text-xs font-semibold text-center">TV</p>
+          <div className="flex h-full w-full items-center relative bg-black overflow-hidden">
+            <Image src="/icons/tv.png" layout="fill" alt="phone" />
+          </div>
+          <p className="text-xs font-semibold text-center">Tv</p>
         </Link>
 
         <Link
           href="/power"
-          className="w-full h-32 border bg-white rounded-md flex flex-col items-center p-3"
+          className="w-full h-32 border mb-3 bg-white rounded-md flex flex-col items-center pb-2 overflow-hidden mx-3"
         >
-          <div className="flex flex-1"></div>
+          <div className="flex h-full w-full items-center relative bg-black overflow-hidden">
+            <Image src="/icons/power.png" layout="fill" alt="phone" />
+          </div>
           <p className="text-xs font-semibold text-center">Electricity</p>
         </Link>
       </div>
@@ -31,9 +40,11 @@ const BillsCards = () => {
       <div className="flex w-full flex-row ">
         <Link
           href="/water"
-          className="w-full h-32 border bg-white rounded-md flex flex-col items-center p-3"
+          className="w-full h-32 border  bg-white rounded-md flex flex-col items-center pb-2 overflow-hidden "
         >
-          <div className="flex flex-1"></div>
+          <div className="flex h-full w-full items-center relative bg-black overflow-hidden">
+            <Image src="/icons/water.png" layout="fill" alt="phone" />
+          </div>
           <p className="text-xs font-semibold text-center">Water</p>
         </Link>
 
