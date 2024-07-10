f07ee1d7959aa5d0129a4bad42a4df275ddcf53c
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2024-07-10 13:44:32 +0300

    chore: Add lazy loading for images in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useAccount } from "wagmi";
 
@@ -12,11 +13,16 @@ export default function Home() {
 
   return (
     <main className="flex items-center justify-center bg-defaultColor h-screen flex-col p-8">
-      <div className="flex flex-1 items-center justify-end flex-col py-10">
-        <h1 className="text-xl font-semibold my-3">
-          Pay for <span className="underline">everyday</span> bills
-        </h1>
+      <div className="flex flex-1 items-center justify-evenly flex-col py-10">
+        <div className="w-full h-full relative ">
+          <Image src="/icons/relax.svg" layout="fill" alt="phone" />
+        </div>
 
+        <div className="flex flex-col mb-3 items-center">
+          <h1 className="text-xl font-bold ">
+            Pay for your <span className="underline">everyday</span> bills
+          </h1>
+        </div>
         <p className="text-[11px] text-white">
           {address?.slice(0, 3)} ....{address?.slice(-4)}
         </p>
